2dc542c71a24569e9ab85d898a0e875201f86a35
Ailton Rocha Cordeiro <arc48388528@gmail.com>
2025-11-18 21:22:46 -0300

    update aula

---
+++ Projetos/Aula02-op.aritimetica/script.js
@@ -0,0 +1,10 @@
+console.log( 2 + 5); // soma
+console.log( 5 - 8); // subtrai 
+console.log( 4 * 8); // Multiplica
+console.log( 5/3 ); // divisao
+console.log( 20 % 2 ); // resto 
+console.log( 20 % 1 ); // resto 
+
+//operaçoes em JS, podendo usar o typeof, pra ver o tipo, no caso NUMBER.
+
+//outros operadores, que nao sao número mas o tipo e NUMBER, sria eles, Ifinity, -Ifinity e NaN (Not A Number).
---
+++ Projetos/Aula03- Strings/script.js
@@ -0,0 +1,5 @@
+//strings 
+//String = texto
+console.log(typeof 'isso é ums string');
+console.log(typeof "Isso também");
+console.log(typeof `Esse também`);
